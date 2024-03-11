7b1f6deb5d2fae9f00408d63974ad88751d33a73
princekhunt <info@princekhunt.com>
2024-03-11 23:16:02 +0530

    allow some files on mobile devices

--- registration/middleware.py
+++ registration/middleware.py
@@ -8,7 +8,7 @@ class AllowDesktopOnlyMiddleware:
 
         if request.user_agent.is_mobile or request.user_agent.is_tablet:
             #if it's not homepage
-            if request.path != "/":
+            if request.path != "/" or request.path != "/robots.txt" or request.path != "/humans.txt" or request.path != "/favicon.ico":
                 return HttpResponseForbidden("<h2 style='margin-top:50%; text-align:center;'>Sorry, PrivatePing works only on desktop. Please use a desktop to get the best experience!</h2>")
             else:
                 return self.get_response(request)
