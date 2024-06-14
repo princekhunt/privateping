ce9b6a8c86ec45635eed201bffe4428c5ceba211
HWKDS <yomikka5555@gmail.com>
2024-06-15 02:29:46 +0530

    username update acoording to issue#14

--- assets/js/registration/Login.js
+++ assets/js/registration/Login.js
@@ -3,26 +3,28 @@ document
   .getElementById("username-button")
   .addEventListener("click", function (event) {
     event.preventDefault();
-
-    document.getElementById("username-form").style.transform =
-      "translateX(-400%)";
-    document.getElementById("username-form").style.transition =
-      "transform 0.5s ease-in-out";
-    //wait
-    setTimeout(function () {
-      document.getElementById("username-form").style.display = "none";
-      document.getElementById("password-field").style.display = "flex";
-      document.getElementById("password-field").style.transform =
-        "translateX(300%)";
-      document.getElementById("password-field").style.transition =
-        "transform 0.4s ease-in-out ";
-      setTimeout(function () {
-        document.getElementById("password-field").style.transform =
-          "translateX(0%)";
-        document.getElementById("password").focus();
-      }, 200);
-    }, 400);
-  });
+    const username = document.getElementById("username").value.trim();
+    if (username == "") {
+      document.getElementById("err_mssg").innerHTML = "user is required!";
+      return false 
+    }
+    else {   
+        document.getElementById("err_mssg").innerHTML = ""; 
+        document.getElementById("username-form").style.transform =
+          "translateX(-400%)";
+        document.getElementById("username-form").style.transition =
+          "transform 0.5s ease-in-out";
+        setTimeout(function () {
+          document.getElementById("username-form").style.display = "none";
+          document.getElementById("password-field").style.display = "flex";
+          document.getElementById("password-field").style.transform = "translateX(300%)";
+          document.getElementById("password-field").style.transition = "transform 0.4s ease-in-out ";
+            setTimeout(function () {
+                document.getElementById("password-field").style.transform = "translateX(0%)";
+                document.getElementById("password").focus();
+              }, 200);
+        }, 400);
+  }});
 
 //form submit
 
