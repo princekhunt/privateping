81f2ac05375edf301a984645c79f8641babd1e32
snipher <sniphermarube@gmail.com>
2024-06-15 12:26:08 +0300

    admin customization

--- chat/admin.py
+++ chat/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
-from .models import *
+from .models import UserProfile, Friends, Keys
 
-admin.site.register(UserProfile)
-admin.site.register(Friends)
-admin.site.register(Keys)
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'username', 'online', 'online_for')
+    search_fields = ('name', 'username')
+    list_filter = ('online', 'online_for')
+    autocomplete_fields = ('online_for', 'user')
+
+@admin.register(Friends)
+class FriendsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'friend', 'note', 'accepted')
+    search_fields = ('user', 'friend')
+    list_filter = ('accepted', )
+    autocomplete_fields = ('user', 'friend')
+
+@admin.register(Keys)
+class KeysAdmin(admin.ModelAdmin):
+    list_display = ('user', 'public_key')
+    search_fields = ('user', )
+    autocomplete_fields = ('user', )
+    
 
 
 admin.site.site_header = "PrivatePing Admin"
--- chat/models.py
+++ chat/models.py
@@ -9,6 +9,11 @@ class UserProfile(models.Model):
     username = models.CharField(max_length=20, unique=True)
     online = models.IntegerField(default=0)
     online_for = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'User Profile'
+        verbose_name_plural = 'User Profiles'
 
     def __str__(self):
         return f"{self.username}"
@@ -19,8 +24,13 @@ class Friends(models.Model):
     id = models.AutoField(primary_key=True, unique=True, editable=False, blank=True)
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     friend = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="friend")
-    note = models.CharField(max_length=100, blank=True, default="")
+    note = models.TextField(null=True, blank=True)
     accepted = models.BooleanField(default=False)
+    
+    class Meta:
+        ordering = ['user', 'friend']
+        verbose_name = 'Friend'
+        verbose_name_plural = 'Friends'
 
     def __str__(self):
         return f"{self.user} - {self.friend}"
@@ -31,6 +41,11 @@ class Keys(models.Model):
     id = models.AutoField(primary_key=True, unique=True, editable=False, blank=True)
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     public_key = models.TextField()
+    
+    class Meta:
+        ordering = ['user']
+        verbose_name = 'Key'
+        verbose_name_plural = 'Keys'
 
     def __str__(self):
         return f"{self.user}"
