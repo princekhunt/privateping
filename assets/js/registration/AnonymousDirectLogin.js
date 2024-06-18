7af9ded8101c917e13324b220ebb727dae7f46bf
Mayureshd-18 <mayuresh.dharwadkar211@vit.edu>
2024-06-18 11:25:04 +0530

    Added hcaptcha

--- assets/js/registration/AnonymousDirectLogin.js
+++ assets/js/registration/AnonymousDirectLogin.js
@@ -1,4 +1,4 @@
-//get cookie
+// Get cookie
 function getCookie(name) {
   var cookieValue = null;
   if (document.cookie && document.cookie !== "") {
@@ -6,7 +6,7 @@ function getCookie(name) {
     for (var i = 0; i < cookies.length; i++) {
       var cookie = cookies[i].trim();
       // Does this cookie string begin with the name we want?
-      if (cookie.substring(0, name.length + 1) === name + "=") {
+      if (cookie.substring(0, name.length + 1) === (name + "=")) {
         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
         break;
       }
@@ -15,43 +15,32 @@ function getCookie(name) {
   return cookieValue;
 }
 
-var requestSent = false; // Flag to track if request has been sent
+$(document).ready(function() {
+  var requestSent = false; // Flag to track if request has been sent
 
-function sendRequest() {
-  if (!requestSent) {
-    requestSent = true; // Set flag to true to indicate request has been sent
-    $.ajax({
-      type: "POST",
-      url: "",
-      data: {
-        csrfmiddlewaretoken: getCookie("csrftoken"),
-        action: "create",
-      },
-      success: function (data) {
-        if (data.status == "ok") {
-          $("#processing").html("Redirecting to Dashboard...");
-          window.location = data.redirect;
+  function sendRequest() {
+    if (!requestSent) {
+      requestSent = true; // Set flag to true to indicate request has been sent
+      $.ajax({
+        type: "POST",
+        url: $("#anonymous-direct-login-form").attr('action'),
+        data: $("#anonymous-direct-login-form").serialize(),
+        headers: {
+          'X-CSRFToken': getCookie("csrftoken")
+        },
+        success: function(data) {
+          if (data.status === "ok") {
+            $("#processing").html("Redirecting to Dashboard...");
+            window.location.href = data.redirect;
+          }
+        },
+        error: function(xhr, status, error) {
+          console.error("An error occurred:", error);
+          alert("An error occurred. Please try again.");
         }
-      },
-    });
+      });
+    }
   }
-}
 
-var mouseMoved = false;
-
-function mouseMoveFunction(e) {
-  mouseMoved = true;
-  $("#processing").html("Anonymously Logging you in...");
-  document.removeEventListener("mousemove", mouseMoveFunction);
-  setTimeout(sendRequest, 3000);
-}
-
-document.addEventListener("mousemove", mouseMoveFunction);
-
-setTimeout(function () {
-  if (mouseMoved && !requestSent) {
-    sendRequest();
-  }
-}, 1000);
-
-parent.document.title = "PrivatePing - Anonymous Direct Login";
+  parent.document.title = "PrivatePing - Anonymous Direct Login";
+});
