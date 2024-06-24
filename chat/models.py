4d070bd1b11dd937b1567f203fe539c41687ff59
snipher <sniphermarube@gmail.com>
2024-06-24 05:53:38 +0300

    Add class Meta to Models to Change Verbose Names

--- chat/models.py
+++ chat/models.py
@@ -21,6 +21,9 @@ class Friends(models.Model):
     friend = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="friend")
     note = models.CharField(max_length=100, blank=True, default="")
     accepted = models.BooleanField(default=False)
+    
+    class Meta:
+        verbose_name_plural = "Friends"
 
     def __str__(self):
         return f"{self.user} - {self.friend}"
@@ -31,6 +34,9 @@ class Keys(models.Model):
     id = models.AutoField(primary_key=True, unique=True, editable=False, blank=True)
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     public_key = models.TextField()
+    
+    class Meta:
+        verbose_name_plural = "Keys"
 
     def __str__(self):
         return f"{self.user}"
