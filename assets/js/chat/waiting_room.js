ffef026a443683b926968a7b0198267026c2cc4f
princekhunt <info@princekhunt.com>
2024-07-29 13:24:22 +0530

    moving scripts to js file,improving codebase.

--- assets/js/chat/waiting_room.js
+++ assets/js/chat/waiting_room.js
@@ -45,6 +45,23 @@ $(document).ready(function(){
     }, 1000); // Check every 1 seconds, adjust as needed
 });
 
+// Add dots to waiting text
+    var text = document.getElementById("waiting_text");
+    var old = document.getElementById("waiting_text").innerHTML;
+    var dots = 0;
+    var dots_interval = setInterval(function () {
+        if (dots < 3) {
+            text.innerHTML += ".";
+            dots++;
+        } else {
+            document.getElementById("waiting_text").innerHTML = old;
+            dots = 0;
+        }
+    }, 500);
+
+
+
+
 //goback
 function goback(){
     window.history.back();
