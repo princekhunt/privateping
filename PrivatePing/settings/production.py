e48f9ea261643a3047a47e83a72427becb1f668c
princekhunt <info@princekhunt.com>
2025-03-06 10:11:04 +0530

    update production settings for better readability

--- PrivatePing/settings/production.py
+++ PrivatePing/settings/production.py
@@ -1,5 +1,4 @@
 from .base import *
-
 import os
 import sentry_sdk
 import dj_database_url
@@ -7,16 +6,18 @@ import django_heroku
 
 django_heroku.settings(locals())
 
+DEBUG = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-DEBUG = False
 ALLOWED_HOSTS = ['privateping.bytespot.tech', 'dev.privateping.bytespot.tech']
+SECRET_ADMIN_URL = os.environ.get('SECRET_ADMIN_URL')
 
+#hcaptcha config
 HCAPTCHA_SITEKEY = os.environ.get('HCAPTCHA_SITEKEY')
 HCAPTCHA_SECRET = os.environ.get('HCAPTCHA_SECRET')
 
+#security config
 SECURE_HSTS_SECONDS = 60
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_SSL_REDIRECT = True
@@ -25,14 +26,14 @@ CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_HSTS_PRELOAD = True
 
-SECRET_ADMIN_URL = os.environ.get('SECRET_ADMIN_URL')
+#sentry config
 SENTRY_DSN = os.environ.get('SENTRY_DSN')
-
 sentry_sdk.init(
     dsn=SENTRY_DSN,
     enable_tracing=True,
 )
 
+#channels config
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
@@ -43,6 +44,7 @@ CHANNEL_LAYERS = {
     }
 }
 
+#database config
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 DATABASES['default']['CONN_MAX_AGE'] = 60
